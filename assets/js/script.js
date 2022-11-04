9cd5b4d3afde98c277498bf2317e822a0c8c931d
c-argus <camila.argus88@gmail.com>
2022-11-04 09:16:12 +0000

    Add favicon

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const question = document.querySelector('#question');
+const answer = Array.from(document.querySelector('.answer-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availabeQuestions= []
+
+let question = [
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        choice1: 'Rice and Beans',
+        choice2: 'Salt and Vinegar',
+        choice3: 'Egg and Friend',
+        choice4: 'Eletronic and Friend',
+        answer: 3,
+    },
+]
