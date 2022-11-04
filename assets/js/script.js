dbfc1be5c6ed518e198521c3c5133fc002d81b2b
c-argus <camila.argus88@gmail.com>
2022-11-04 10:44:24 +0000

    Add questions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,87 @@ let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
-let availabeQuestions= []
+let availabeQuestions = []
 
 let question = [
     {
+        question: "Which video game was the first video game played in space during the 1990’s?",
+        option1: "Pac Man",
+        option2: "Tetris",
+        option3: "Uno",
+        option4: "Super Mario",
+        answer: 2,
+    },
+    {
         question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
-        choice1: 'Rice and Beans',
-        choice2: 'Salt and Vinegar',
-        choice3: 'Egg and Friend',
-        choice4: 'Eletronic and Friend',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
+        answer: 3,
+    },
+    {
+        question: 'Tamagotchi’s were all the rage in the 1990’s. But what do the two Japanese words translate to?',
+        option1: 'Rice and Beans',
+        option2: 'Salt and Vinegar',
+        option3: 'Egg and Friend',
+        option4: 'Eletronic and Friend',
         answer: 3,
     },
 ]
