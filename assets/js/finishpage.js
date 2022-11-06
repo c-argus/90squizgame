a8e81fd2368e8d92d2d9cfcf915bbb88e2d92e85
c-argus <camila.argus88@gmail.com>
2022-11-06 09:23:26 +0000

    Add local storage for leaderboard js

--- assets/js/finishpage.js
+++ assets/js/finishpage.js
@@ -1,4 +1,7 @@
 var nickname = document.getElementById('nickname')
 var scorebtn = document.getElementById('score-btn')
 var finalScore = document.getElementById('nickname')
-var nickname = document.getElementById('nickname')
+var currentScore = document.getElementById('currentScore')
+
+var scores = JSON.parse(localStorage.getItem('scores')) || []
+
