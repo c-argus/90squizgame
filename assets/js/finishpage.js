603c78ceaa237dbb74116bcf4a0a0df81c9f8471
c-argus <camila.argus88@gmail.com>
2022-11-06 09:17:34 +0000

    Add var final page js

---
+++ assets/js/finishpage.js
@@ -0,0 +1,4 @@
+var nickname = document.getElementById('nickname')
+var scorebtn = document.getElementById('score-btn')
+var finalScore = document.getElementById('nickname')
+var nickname = document.getElementById('nickname')
