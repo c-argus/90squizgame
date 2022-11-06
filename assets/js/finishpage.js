0f5a9d433ad12868e6afcf007346ce1da0c2794a
c-argus <camila.argus88@gmail.com>
2022-11-06 09:35:01 +0000

    Add score js

--- assets/js/finishpage.js
+++ assets/js/finishpage.js
@@ -5,3 +5,29 @@ var currentScore = document.getElementById('currentScore')
 
 var scores = JSON.parse(localStorage.getItem('scores')) || []
 
+var maxHighScores = 5
+
+finalScore.innerText = currentScore
+
+nickname.addEventListener('keyup', () => {
+    scorebtn.disabled = !nickname.value 
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score = {
+        score: currentScore,
+        name: nickname.value
+    }
+
+    scores.push(score)
+
+    scores.sort(a,b) => {
+        return b.score - a.score
+    }
+
+    scores.splice(5)
+    localStorage.setItem('scores', JSON.stringify(scores))
+    window.location.assign('/')
+}
