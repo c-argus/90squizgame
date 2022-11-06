e25a362c3bdee05a45fe26104f95d0101378a443
c-argus <camila.argus88@gmail.com>
2022-11-06 11:23:54 +0000

    Add changes on html and js

---
+++ assets/js/game.js
@@ -0,0 +1,76 @@
+var question = document.getElementById('question');
+var answerText = Array.from(document.getElementsByClassName('answer-text'));
+var progressNumber = document.getElementById('progressNumber');
+var scoreText = document.getElementById('main-score');
+var progressBarFull = document.getElementById('progressBarFull');
+console.log(progressNumber);
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+var ScorePoints = 100
+let TotalQuestions = 10
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...MyQuestions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > TotalQuestions) {
+        localStorage.setItem('currentScore', score)
+
+        return window.location.assign("finishpage.html")
+    }
+
+    questionCounter++
+    progressNumber.innerText = `Question ${questionCounter} of ${TotalQuestions}`
+    progressBarFull.style.width = `${(questionCounter/TotalQuestions) * 100}%`
+
+    const MyQuestionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[MyQuestionsIndex]
+    question.innerText = currentQuestion.question
+
+    answerText.forEach(option => {
+        const number = option.dataset["number"]
+        option.innerText = currentQuestion["option" + number]
+    })
+
+    availableQuestions.splice(MyQuestionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+answerText.forEach(option => {
+    option.addEventListener("click", e => {
+        if (!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedOption = e.target
+        const selectedAnswer = selectedOption.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+        if (classToApply == 'correct') {
+            incrementScore(ScorePoints)
+        }
+
+        selectedOption.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedOption.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+startGame()
