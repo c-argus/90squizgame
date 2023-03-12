c5866ce3a68db5d94fb0f84a70eabaa93233d512
c-argus <camila.argus88@gmail.com>
2023-03-12 14:12:10 +0000

    Highlighted what's from the tutorial video for future edits

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,7 +1,7 @@
 var highScoresList = document.getElementById('highScoresList')
 var scores = JSON.parse(localStorage.getItem('scores')) || []
 
-highScoresList.innerHTML = 
-scores.map(score => {
-    return `<li class="high-score">${score.name} - ${score.scoreText}</li>`
-}).join('')
+highScoresList.innerHTML = // from the video
+scores.map(score => {   // from the video
+    return `<li class="high-score">${score.name} - ${score.scoreText}</li>`   // from the video
+}).join('') // from the video
