9dafb10a7e02954eb0662d5d8b42b3ab0657d822
c-argus <camila.argus88@gmail.com>
2023-03-12 14:10:20 +0000

    Highlighted what came from the tutorial video for future edit

--- assets/js/finishpage.js
+++ assets/js/finishpage.js
@@ -9,25 +9,25 @@ var completedAudio = new Audio('assets/audioClips/completedAudio.wav');
 
 finalScore.innerText = currentScore.toString();
 
-nickname.addEventListener('keyup', () => {
-    scorebtn.disabled = !nickname.value; 
+nickname.addEventListener('keyup', () => { // from the video
+    scorebtn.disabled = !nickname.value;     // from the video
 });
 
-saveScore = e => {
-    e.preventDefault();
+saveScore = e => {        // from the video
+    e.preventDefault();     // from the video
 
-    const scoreText = {
-        scoreText: currentScore,
-        name: nickname.value,
+    const scoreText = {     // from the video
+        scoreText: currentScore, // from the video
+        name: nickname.value,   // from the video
     };
 
-    scores.push(scoreText);
+    scores.push(scoreText); // from the video
 
-    scores.sort((a,b) => {
-        return b.scoreText - a.scoreText;
+    scores.sort((a,b) => {          // from the video
+        return b.scoreText - a.scoreText;  // from the video
     });
 
-    scores.splice(5);
-    localStorage.setItem('scores', JSON.stringify(scores));
-    window.location.assign('./scorespage.html');
+    scores.splice(5); // from the video
+    localStorage.setItem('scores', JSON.stringify(scores)); // from the video
+    window.location.assign('./scorespage.html'); // from the video
 };
